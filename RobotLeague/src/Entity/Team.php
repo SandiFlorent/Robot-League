4bb2640d1e43d4d07f9d5548248bae9a9b23bb62
Sandi Florent <mahafalimaminirina.florent@etu.u-bordeaux.fr>
2025-01-06 17:14:23 +0100

    Rajout des entités et controlleurs nécessaire
    Rajout de l'entity User, d'une connexion des Users, d'entity Team et des TeamMembers

---
+++ RobotLeague/src/Entity/Team.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TeamRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TeamRepository::class)]
+class Team
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Name = null;
+
+    /**
+     * @var Collection<int, Member>
+     */
+    #[ORM\ManyToMany(targetEntity: Member::class, inversedBy: 'teams')]
+    private Collection $TeamMembers;
+
+    public function __construct()
+    {
+        $this->TeamMembers = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->Name;
+    }
+
+    public function setName(string $Name): static
+    {
+        $this->Name = $Name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Member>
+     */
+    public function getTeamMembers(): Collection
+    {
+        return $this->TeamMembers;
+    }
+
+    public function addTeamMember(Member $teamMember): static
+    {
+        if (!$this->TeamMembers->contains($teamMember)) {
+            $this->TeamMembers->add($teamMember);
+        }
+
+        return $this;
+    }
+
+    public function removeTeamMember(Member $teamMember): static
+    {
+        $this->TeamMembers->removeElement($teamMember);
+
+        return $this;
+    }
+}
