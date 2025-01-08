d37f852772d5b1306d89d2a28eebdd36a69b6df6
Theo Chabane <theo.chabane@etu.u-bordeaux.fr>
2025-01-08 09:48:40 +0100

    ajout de l'affichage du championnat au public

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ChampionshipRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -15,4 +16,16 @@ class HomeController extends AbstractController
             'controller_name' => 'HomeController',
         ]);
     }
+
+    #[Route('/', name: 'app_home', methods: ['GET'])]
+    public function home(ChampionshipRepository $championshipRepository): Response
+    {
+        // Récupère toutes les rencontres
+        $championships = $championshipRepository->findAll();
+
+        return $this->render('home/index.html.twig', [
+            'championships' => $championships,  // Envoie les championnats au template
+        ]);
+    }
+
 }
