120fae8240630890f7217367299ab84ca55e966c
Theo Chabane <theo.chabane@etu.u-bordeaux.fr>
2025-01-10 15:16:31 +0100

    ajout du choix de championnat à l'acceuil

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,25 +6,37 @@ use App\Repository\ChampionshipRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\Championship;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use App\Repository\ChampionshipListRepository;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
-    {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
-    }
+    public function index(
+        Request $request,
+        ChampionshipRepository $championshipRepository,
+        ChampionshipListRepository $championshipListRepository
+    ): Response {
+        // Récupérer l'ID du championnat sélectionné dans la requête
+        $championshiplistId = $request->query->get('championshiplist_id');
 
-    #[Route('/{_locale}', name: 'app_home', methods: ['GET'])]
-    public function home(ChampionshipRepository $championshipRepository): Response
-    {
-        // Récupère toutes les rencontres
-        $championships = $championshipRepository->findAll();
+        // Si un championnat est sélectionné, récupérer les matchs correspondants
+        if ($championshiplistId) {
+            $championships = $championshipRepository->findBy(['championshipList' => $championshiplistId]);
+        } else {
+            // Sinon, récupérer tous les matchs
+            $championships = $championshipRepository->findAll();
+        }
+
+        // Récupérer la liste des championnats disponibles
+        $championshipLists = $championshipListRepository->findAll();
 
         return $this->render('home/index.html.twig', [
-            'championships' => $championships,  // Envoie les championnats au template
+            'championships' => $championships,
+            'championshipLists' => $championshipLists,
+            'selected_championship_id' => $championshiplistId, // Passer l'ID sélectionné (null si non sélectionné)
         ]);
     }
 
