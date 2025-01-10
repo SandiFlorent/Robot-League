5a6c32146efaa6a21f3a7372f85538e317876f7f
Theo Chabane <theo.chabane@etu.u-bordeaux.fr>
2025-01-10 12:07:54 +0100

    possibilités de choisir le championnat pour team et championnat

--- src/Form/TeamType.php
+++ src/Form/TeamType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Member;
 use App\Entity\Team;
+use App\Entity\ChampionshipList;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -19,6 +20,11 @@ class TeamType extends AbstractType
                 'class' => 'border rounded-lg p-2 w-full', // Exemple de classes Tailwind
                 'placeholder' => 'Nom de l\'équipe'
             ]
+        ])
+        ->add('championshipList', EntityType::class, [
+            'class' => ChampionshipList::class, // La classe ChampionshipList
+            'choice_label' => 'ChampionshipName',  // Affiche le nom du championnat
+            'label' => 'Choisir un championnat', // Libellé du champ
         ]);
 }
 }
