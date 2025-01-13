a5dfdd9847730473e1f4d429ff44c27540b3caea
saisinh yohan <yohan.saisin@etu.u-bordeaux.fr>
2025-01-13 14:11:26 +0100

    full css sur les pages

--- src/Form/ChampionshipListType.php
+++ src/Form/ChampionshipListType.php
@@ -12,17 +12,56 @@ class ChampionshipListType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('ChampionshipName')
-            ->add('description')
-            ->add('adress')
+            ->add('ChampionshipName', null, [
+                'attr' => [
+                    'class' => 'w-full px-4 py-2 border border-gray-600 bg-gray-800 text-white rounded-md'
+                ],
+                'label_attr' => [
+                    'class' => 'block text-lg font-semibold text-white mb-2'
+                ]
+            ])
+            ->add('description', null, [
+                'attr' => [
+                    'class' => 'w-full px-4 py-2 border border-gray-600 bg-gray-800 text-white rounded-md'
+                ],
+                'label_attr' => [
+                    'class' => 'block text-lg font-semibold text-white mb-2'
+                ]
+            ])
+            ->add('adress', null, [
+                'attr' => [
+                    'class' => 'w-full px-4 py-2 border border-gray-600 bg-gray-800 text-white rounded-md'
+                ],
+                'label_attr' => [
+                    'class' => 'block text-lg font-semibold text-white mb-2'
+                ]
+            ])
             ->add('dateStart', null, [
                 'widget' => 'single_text',
+                'attr' => [
+                    'class' => 'w-full px-4 py-2 border border-gray-600 bg-gray-800 text-white rounded-md'
+                ],
+                'label_attr' => [
+                    'class' => 'block text-lg font-semibold text-white mb-2'
+                ]
             ])
             ->add('dateEnd', null, [
                 'widget' => 'single_text',
+                'attr' => [
+                    'class' => 'w-full px-4 py-2 border border-gray-600 bg-gray-800 text-white rounded-md'
+                ],
+                'label_attr' => [
+                    'class' => 'block text-lg font-semibold text-white mb-2'
+                ]
             ])
-            ->add('threshold')
-        ;
+            ->add('threshold', null, [
+                'attr' => [
+                    'class' => 'w-full px-4 py-2 border border-gray-600 bg-gray-800 text-white rounded-md'
+                ],
+                'label_attr' => [
+                    'class' => 'block text-lg font-semibold text-white mb-2'
+                ]
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
