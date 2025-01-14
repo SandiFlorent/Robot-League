6f6c2e17f8fce2701bc1e75bcf4486ddaf2fc484
saisinh yohan <yohan.saisin@etu.u-bordeaux.fr>
2025-01-14 18:01:21 +0100

    REUSSI REPARATION

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -20,41 +20,52 @@ class HomeController extends AbstractController
         Request $request,
         ChampionshipRepository $championshipRepository,
         ChampionshipListRepository $championshipListRepository,
-        FieldRepository $fieldRepository,  // Ajout de FieldRepository
-        SlotRepository $slotRepository     // Ajout de SlotRepository
+        FieldRepository $fieldRepository,
+        SlotRepository $slotRepository
     ): Response {
-        // Récupérer les paramètres de la requête
         $championshiplistId = $request->query->get('championshiplist_id');
         $fieldId = $request->query->get('field_id');
         $slotId = $request->query->get('slot_id');
-    
-        // Récupérer la liste des championnats
+        $page = max(1, (int) $request->query->get('page', 1));
+        $limit = 6; // Nombre de résultats par page
+
         $championshipLists = $championshipListRepository->findAll();
         $fields = [];
         $slots = [];
-    
+
         if ($championshiplistId) {
-            // Récupérer les terrains et créneaux associés au championnat sélectionné
             $championshipList = $championshipListRepository->find($championshiplistId);
             $fields = $fieldRepository->findBy(['championshipList' => $championshipList]);
             $slots = $slotRepository->findBy(['championshipList' => $championshipList]);
-    
-            // Récupérer les matchs filtrés par le championnat, terrain et créneau
+
             $criteria = ['championshipList' => $championshiplistId];
-    
             if ($fieldId) {
                 $criteria['field'] = $fieldId;
             }
             if ($slotId) {
                 $criteria['slot'] = $slotId;
             }
-    
-            $championships = $championshipRepository->findBy($criteria);
+
+            $championships = $championshipRepository->findBy(
+                $criteria,
+                ['id' => 'ASC'],
+                $limit,
+                ($page - 1) * $limit
+            );
+
+            $totalItems = $championshipRepository->count($criteria);
         } else {
-            // Si aucun championnat n'est sélectionné, récupérer tous les matchs
-            $championships = $championshipRepository->findAll();
+            $championships = $championshipRepository->findBy(
+                [],
+                ['id' => 'ASC'],
+                $limit,
+                ($page - 1) * $limit
+            );
+            $totalItems = $championshipRepository->count([]);
         }
-    
+
+        $totalPages = (int) ceil($totalItems / $limit);
+
         return $this->render('home/index.html.twig', [
             'championships' => $championships,
             'championshipLists' => $championshipLists,
@@ -63,6 +74,8 @@ class HomeController extends AbstractController
             'selected_championship_id' => $championshiplistId,
             'selected_field_id' => $fieldId,
             'selected_slot_id' => $slotId,
+            'page' => $page,
+            'totalPages' => $totalPages
         ]);
     }
     
