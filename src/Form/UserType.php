d2f55a711b4d890a79052abdf26f79e94b4830a0
Theo Chabane <theo.chabane@etu.u-bordeaux.fr>
2025-01-14 09:14:54 +0100

    suppression d'un creator id d'une team

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -18,9 +18,51 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            // Sélection du championnat (non lié à l'entité User)
+            ->add('championship', EntityType::class, [
+                'class' => ChampionshipList::class,
+                'choice_label' => 'championshipName',
+                'placeholder' => 'Sélectionner un championnat',
+                'mapped' => false,  
+                'required' => true
+            ])
+            
+            ->add('myTeams', EntityType::class, [
+                'class' => Team::class,
+                'choice_label' => 'name',
+                'multiple' => true,  // Gestion de la collection
+                'expanded' => false,
+                'required' => false, // Pas obligatoire lors du rechargement
+                'query_builder' => function (EntityRepository $er) use ($options) {
+                    if (!empty($options['championship_id']) && isset($options['user'])) {
+                        return $er->createQueryBuilder('t')
+                            ->where('t.championshipList = :id')
+                            ->andWhere('(t.creator IS NULL OR t.creator = :user)')
+                            ->setParameter('id', $options['championship_id'])
+                            ->setParameter('user', $options['user']->getId());
+                    }
+                    return $er->createQueryBuilder('t')->where('1=0');
+                },
+                'choice_value' => function (?Team $team) {
+                    return $team ? (string)$team->getId() : '';
+                }
+            ])        
+
+            // Option pour retirer le créateur
+            ->add('removeCreator', ChoiceType::class, [
+                'label' => 'Retirer le rôle de créateur',
+                'choices' => ['Oui' => true, 'Non' => false],
+                'mapped' => false,
+                'required' => false,
+                'expanded' => true,
+                'disabled' => empty($options['userTeam']),
+            ])
+
+            // Champs standards
             ->add('email')
             ->add('password', PasswordType::class, [
-                'label' => 'Mot de passe'
+                'mapped' => false,
+                'required' => false
             ])
             ->add('roles', ChoiceType::class, [
                 'choices' => [
@@ -30,63 +72,7 @@ class UserType extends AbstractType
                 ],
                 'multiple' => true,
                 'expanded' => true
-            ])
-            ->add('championship', EntityType::class, [
-                'class' => ChampionshipList::class,
-                'choice_label' => 'championshipName',
-                'placeholder' => 'Sélectionner un championnat',
-                'mapped' => false,  // Ne lie pas directement à l'entité User
-                'attr' => ['onchange' => 'this.form.submit()']  // Recharge le formulaire au changement
-            ])
-            ->add('myTeams', EntityType::class, [
-                'class' => Team::class,
-                'choice_label' => 'name',
-                'multiple' => false,
-                'expanded' => false,
-                'query_builder' => function (EntityRepository $er) use ($options) {
-                    if (!empty($options['championship_id']) && isset($options['user'])) {
-                        // Vérification si l'utilisateur a déjà une équipe dans ce championnat
-                        $userTeam = $er->createQueryBuilder('t')
-                            ->where('t.championshipList = :id')
-                            ->andWhere('t.creator = :user')
-                            ->setParameter('id', $options['championship_id'])
-                            ->setParameter('user', $options['user']->getId())
-                            ->getQuery()
-                            ->getOneOrNullResult();
-            
-                        if ($userTeam) {
-                            // Affiche uniquement l'équipe existante de l'utilisateur
-                            return $er->createQueryBuilder('t')
-                                ->where('t.id = :teamId')
-                                ->setParameter('teamId', $userTeam->getId());
-                        } else {
-                            // Sinon, affiche les équipes sans créateur
-                            return $er->createQueryBuilder('t')
-                                ->where('t.championshipList = :id')
-                                ->andWhere('t.creator IS NULL')
-                                ->setParameter('id', $options['championship_id']);
-                        }
-                    } else {
-                        return $er->createQueryBuilder('t')->where('1=0'); 
-                    }
-                },
-                'choice_value' => function ($choice) {
-                    return $choice instanceof \App\Entity\Team ? $choice->getId() : null;
-                },
-                'label' => 'Sélectionner une équipe'
-            ])
-            ->add('removeCreator', ChoiceType::class, [
-                'label' => 'Retirer le rôle de créateur de cette équipe',
-                'choices' => [
-                    'Oui' => true,
-                    'Non' => false
-                ],
-                'mapped' => false,
-                'required' => false,
-                'expanded' => true,
-                'disabled' => !isset($options['userTeam'])  // Désactiver si l'utilisateur n'a pas d'équipe
             ]);
-            
     }
 
     public function configureOptions(OptionsResolver $resolver): void
@@ -94,8 +80,8 @@ class UserType extends AbstractType
         $resolver->setDefaults([
             'data_class' => User::class,
             'championship_id' => null,
-            'user' => null, // Ajout de l'utilisateur dans les options
-            'userTeam' => null  // Ajout explicite de l'option userTeam
+            'user' => null,
+            'userTeam' => null
         ]);
     }
 
